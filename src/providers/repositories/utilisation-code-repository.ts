f3b16645f33bc31b405323ec0d0b72d12b807e34
Unknown <syd_freak@hotmail.com>
2018-04-20 09:59:40 -0400

    feat: add utilisationCode repository.

---
+++ src/providers/repositories/utilisation-code-repository.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {HttpService} from '../Base/http.service';
+import {Observable} from 'rxjs/Observable';
+import {map} from 'rxjs/operators';
+import {UtilisationCode} from '../../models/utilisation-code';
+
+@Injectable()
+export class UtilisationCodeRepositoryProvider  {
+
+  constructor(private http: HttpService) {}
+
+  get(idUtilisationCode: string): Observable<UtilisationCode>{
+    if (!idUtilisationCode) {
+      return Observable.of(new UtilisationCode());
+    } else {
+      return this.http.get('utilisationcode/' + idUtilisationCode)
+        .pipe(map(response => {
+          console.log(response);
+          return response;
+        }));
+    }
+  }
+}
