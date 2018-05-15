7bfde6157b6d6cb00f12781ae8edb6a1a2fe8faf
Philippe Robert <philippe.robert@cauca.ca>
2018-05-15 12:45:35 -0400

    feat(#24) : Add validation on navigation and display

--- src/pages/inspection-question/inspection-question.ts
+++ src/pages/inspection-question/inspection-question.ts
@@ -2,6 +2,7 @@ import {Component, ViewChild} from '@angular/core';
 import {IonicPage, NavController, NavParams, Slides} from 'ionic-angular';
 import {InspectionQuestion} from "../../models/inspection-question";
 import {InspectionQuestionRepositoryProvider} from "../../providers/repositories/inspection-question-repository-provider";
+import {AuthenticationService} from "../../providers/Base/authentification.service";
 
 @IonicPage()
 @Component({
@@ -16,25 +17,35 @@ export class InspectionQuestionPage {
     public idInspection: string = '';
     public selectedIndex = 0;
     public currentQuestion: InspectionQuestion = new InspectionQuestion();
-    public previousQuestionAvailable = true;
-    public nexQuestionDisabled = true;
+    public previousQuestionAvailable = false;
+    public nextQuestionDisabled = false;
     public questionTypeEnum = {'MultipleChoice': 1, 'TextAnswer': 2, 'DateAnswer': 3};
     public nextButtonTitle: string = 'Suivante';
     public nextQuestionId: string = '';
     public reviewOnly = false;
-
+    public changingValueTimer = null;
 
     constructor(public navCtrl: NavController,
                 public navParams: NavParams,
                 public controller: InspectionQuestionRepositoryProvider,
+                private authService: AuthenticationService,
     ) {
         this.idInspection = this.navParams.get('idInspection');
         this.loadInspectionQuestion();
     }
 
+    async ionViewCanEnter() {
+        let isLoggedIn = await this.authService.isStillLoggedIn();
+        if (!isLoggedIn)
+            this.redirectToLoginPage();
+    }
+
+    private redirectToLoginPage(){
+        this.navCtrl.setRoot('LoginPage');
+    }
+    
     ionViewDidLoad() {
         this.slides.lockSwipes(true);
-        this.questionNavigationDisplay();
     }
 
     loadInspectionQuestion() {
@@ -43,16 +54,30 @@ export class InspectionQuestionPage {
                 this.inspectionQuestion = result;
                 this.inspectionQuestionAnswer.push(Object.assign([], result[this.selectedIndex]));
                 this.currentQuestion = this.inspectionQuestionAnswer[this.selectedIndex];
-                this.nextQuestionId = this.currentQuestion.idSurveyQuestionNext;
-                if (this.currentQuestion.answer) {
-                    this.checkAlreadyAnswered();
-                }
+                this.canSwitchQuestion();
+                this.getNextQuestionFromAnswer(0);
             });
     }
 
     switchQuestion() {
         this.selectedIndex = this.slides.getActiveIndex();
         this.currentQuestion = this.inspectionQuestionAnswer[this.selectedIndex];
+        this.getNextQuestionFromAnswer();
+        this.canSwitchQuestion();
+    }
+
+    canSwitchQuestion() {
+        let retValue = true;
+        if (this.currentQuestion.answer) {
+            this.nextQuestionDisabled = false;
+            this.reviewOnly = true;
+        } else {
+            this.nextQuestionDisabled = true;
+            this.reviewOnly = false;
+            retValue = false;
+        }
+
+        return retValue;
     }
 
     findQuestion(idSurveyQuestion: string) {
@@ -67,59 +92,69 @@ export class InspectionQuestionPage {
         }
     }
 
+    isNextQuestionExists() {
+        let nextExists = false;
+        const questionCount = this.currentQuestion.choicesList.length;
+        for (let index = 0; index < questionCount; index++) {
+            if (this.currentQuestion.choicesList[index].idSurveyQuestionNext) {
+                nextExists = true;
+                break;
+            }
+        }
+        if (!nextExists) {
+            if (this.currentQuestion.idSurveyQuestionNext) {
+                nextExists = true;
+            }
+        }
+        return nextExists;
+    }
+
     nextQuestion() {
-        this.slides.lockSwipes(false);
-
-        this.checkAlreadyAnswered();
-
-        if (this.nextQuestionId != '') {
+        if (this.nextQuestionId) {
             let nextIndex = this.findQuestion(this.nextQuestionId);
 
             if (!this.inspectionQuestionAnswer[this.selectedIndex + 1]) {
                 this.addNewAnswer(nextIndex);
             }
-
+            this.slides.lockSwipes(false);
             this.slides.slideTo(this.selectedIndex + 1);
             this.switchQuestion();
             this.slides.lockSwipes(true);
-            this.questionNavigationDisplay();
+
         } else {
             this.navCtrl.pop();
         }
     }
 
-    checkAlreadyAnswered() {
-        if (this.inspectionQuestion[this.selectedIndex + 1]) {
-            if (this.inspectionQuestion[this.selectedIndex + 1].answer) {
-                this.reviewOnly = true;
-                this.answerChanged();
-            } else {
-                this.reviewOnly = false;
-            }
-        }
-    }
 
     previousQuestion() {
         this.slides.lockSwipes(false);
         this.slides.slidePrev();
         this.switchQuestion();
         this.slides.lockSwipes(true);
-        this.questionNavigationDisplay();
     }
 
-    answerChanged() {
-        if (this.currentQuestion.questionType == this.questionTypeEnum.MultipleChoice) {
-            if (this.currentQuestion.answer != '') {
-                this.nextQuestionId = this.getChoiceNextQuestionId(this.currentQuestion.answer);
-                if (this.nextQuestionId == '') {
+    getNextQuestionFromAnswer(delay: number = 1500) {
+        if (this.changingValueTimer) {
+            clearTimeout(this.changingValueTimer);
+        }
+        this.changingValueTimer = setTimeout(() => {
+            console.log('changement de valeur de la réponse');
+            if (this.currentQuestion.answer) {
+                if (this.currentQuestion.questionType == this.questionTypeEnum.MultipleChoice) {
+                    this.nextQuestionId = this.getChoiceNextQuestionId(this.currentQuestion.answer);
+                    if (!this.nextQuestionId) {
+                        this.nextQuestionId = this.currentQuestion.idSurveyQuestionNext;
+                    }
+                } else {
                     this.nextQuestionId = this.currentQuestion.idSurveyQuestionNext;
                 }
-                this.nexQuestionDisabled = false;
+                this.nextQuestionDisabled = true;
+            } else{
+                this.nextQuestionDisabled = false;
             }
-        } else {
-            this.nextQuestionId = this.currentQuestion.idSurveyQuestionNext;
-        }
-        this.questionNavigationDisplay();
+            this.questionNavigationDisplay();
+        }, delay);
     }
 
     getChoiceNextQuestionId(idChoiceSelected) {
@@ -143,10 +178,10 @@ export class InspectionQuestionPage {
             this.previousQuestionAvailable = false;
         }
 
-        if (this.nextQuestionId) {
+        if (this.nextQuestionId || this.isNextQuestionExists()) {
             this.nextButtonTitle = 'Suivante';
         } else {
-            this.nextButtonTitle = 'Terminer';
+            this.nextButtonTitle = 'Compléter';
         }
     }
 
