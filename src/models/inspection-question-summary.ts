0b7aeec3c21480c556c060d5637f0421eb24166e
Philippe Robert <philippe.robert@cauca.ca>
2018-05-18 08:10:41 -0400

    feat(#45) : Add new class to group survey summary

--- src/models/inspection-question-summary.ts
+++ src/models/inspection-question-summary.ts
@@ -1,6 +1,9 @@
 export class InspectionQuestionSummary {
     id: string;
+    questionTitle: string;
     questionDescription: string;
     answer: string;
     questionType: number;
+    sequence: number;
+    recursive: boolean;
 }
