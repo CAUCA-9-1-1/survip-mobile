922367cb937ed845ba9731ed80fb1842568772b2
audrey <audrey.lebelr@gmail.com>
2018-07-06 21:39:41 -0400

    (feat#75): Removed pictograms to move them to DB

--- src/components/picture-viewer/picture-viewer.ts
+++ src/components/picture-viewer/picture-viewer.ts
@@ -34,38 +34,8 @@ export class PictureViewerComponent implements ControlValueAccessor, OnDestroy {
   private changed = new Array<(value: string) => void>();
   private touched = new Array<() => void>();
 
-  public iconsPath = './../../../assets/pictograms/';
   private modifiedJson: string;
-  public icons = [
-    'Annonciateur.png',
-    'BoiteClees.png',
-    'BoiteInformationPompier.png',
-    'BorneIncendie.png',
-    'BorneIncendieMurale.png',
-    'BorneIncendiePrivee.png',
-    'BorneIncendieMurale.png',
-    'BorneIncendiePrivee.png',
-    'CamionPompier.png',
-    'EntreeElectrique.png',
-    'EntreeEauDomestique.png',
-    'GazNaturel.png',
-    'GazPropane.png',
-    'Generatrice.png',
-    'MatiereDangereuse.png',
-    'PanneauAlarmeIncendie.png',
-    'PoteauIndicateurDeVanne.png',
-    'RaccordPompierCanalisation.png',
-    'RaccordPompierGicleur.png',
-    'RaccordPompierMixte.png',
-    'ReservoirProduitPetrolier.png',
-    'SoupapeAlarmeSousAir.png',
-    'SouspapeAlarmeSousEau.png',
-    'AccesPrincipal.png',
-    'SensUnique.png',
-    'PompeIncendie.png',
-    'MurFaible.png'
-  ];
-
+ 
   isUsingCordova: boolean;
 
   get imageUrl(){
