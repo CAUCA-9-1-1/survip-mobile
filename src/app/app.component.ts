6909d3026042b0de19ae3105b4a98823b21d12a6
Philippe Robert <philippe.robert@cauca.ca>
2018-07-31 14:55:25 -0400

    feat(#101) Hot fix from rebase on  master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   templateUrl: 'app.html'
@@ -9,12 +10,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   rootPage:any = 'LoginPage';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       if(platform.is('android')) {
         statusBar.styleBlackOpaque();}
       splashScreen.hide();
+        translate.setDefaultLang('en');
+        translate.use('en');
     });
   }
 }
--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,42 +1,45 @@
-import { NgModule } from '@angular/core';
-import { SearchBoxComponent } from './search-box/search-box';
+import {NgModule} from '@angular/core';
+import {SearchBoxComponent} from './search-box/search-box';
 import {FormsModule} from '@angular/forms';
-import { SearchListComponent } from './search-list/search-list';
+import {SearchListComponent} from './search-list/search-list';
 import {IonicPageModule} from 'ionic-angular';
-import { PictureViewerComponent } from './picture-viewer/picture-viewer';
+import {PictureViewerComponent} from './picture-viewer/picture-viewer';
 import {Camera} from '@ionic-native/camera'
-import { IonicImageViewerModule } from 'ionic-img-viewer';
+import {IonicImageViewerModule} from 'ionic-img-viewer';
 import {WindowRefService} from '../providers/Base/window-ref.service';
 import {BuildingChildPicturesComponent} from './building-child-pictures/building-child-pictures';
-import { InspectionBuildingParticularRiskDetailComponent } from './inspection-building-particular-risk-detail/inspection-building-particular-risk-detail';
-import { MenuProfileComponent } from './menu-profile/menu-profile';
+import {InspectionBuildingParticularRiskDetailComponent} from './inspection-building-particular-risk-detail/inspection-building-particular-risk-detail';
+import {MenuProfileComponent} from './menu-profile/menu-profile';
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
-	declarations: [
-	  SearchBoxComponent,
-    SearchListComponent,
-    PictureViewerComponent,
-    BuildingChildPicturesComponent,
-    InspectionBuildingParticularRiskDetailComponent,
-    MenuProfileComponent
-  ],
-	imports: [
-	  FormsModule,
-    IonicPageModule.forChild(SearchBoxComponent),
-    IonicPageModule.forChild(SearchListComponent),
-    IonicImageViewerModule
-  ],
-	exports: [
-	  SearchBoxComponent,
-    SearchListComponent,
-    PictureViewerComponent,
-    BuildingChildPicturesComponent,
-    InspectionBuildingParticularRiskDetailComponent,
-    MenuProfileComponent,
-  ],
-  providers: [
-    WindowRefService,
-    Camera,
-  ]
+    declarations: [
+        SearchBoxComponent,
+        SearchListComponent,
+        PictureViewerComponent,
+        BuildingChildPicturesComponent,
+        InspectionBuildingParticularRiskDetailComponent,
+        MenuProfileComponent
+    ],
+    imports: [
+        FormsModule,
+        IonicPageModule.forChild(SearchBoxComponent),
+        IonicPageModule.forChild(SearchListComponent),
+        IonicImageViewerModule,
+        TranslateModule.forChild(),
+    ],
+    exports: [
+        SearchBoxComponent,
+        SearchListComponent,
+        PictureViewerComponent,
+        BuildingChildPicturesComponent,
+        InspectionBuildingParticularRiskDetailComponent,
+        MenuProfileComponent,
+    ],
+    providers: [
+        WindowRefService,
+        Camera,
+    ]
 })
-export class ComponentsModule {}
+export class ComponentsModule {
+}
