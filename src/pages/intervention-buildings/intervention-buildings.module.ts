9d3a5f6e20d37515347ba92dfac5499d57df7bae
Philippe Robert <philippe.robert@cauca.ca>
2018-07-31 15:55:35 -0400

    feat(#101) Add translation for fire hydrant and building list

--- src/pages/intervention-buildings/intervention-buildings.module.ts
+++ src/pages/intervention-buildings/intervention-buildings.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { InterventionBuildingsPage } from './intervention-buildings';
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { InterventionBuildingsPage } from './intervention-buildings';
   ],
   imports: [
     IonicPageModule.forChild(InterventionBuildingsPage),
+      TranslateModule.forChild()
   ],
   exports: [
     InterventionBuildingsPage
