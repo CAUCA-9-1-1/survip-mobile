d9ac2284f1d0bb6daa193678a31718e981483866
Philippe Robert <philippe.robert@cauca.ca>
2018-08-01 12:02:43 -0400

    feat(#101) Add translation for building detail

--- src/pages/building-details/building-details.module.ts
+++ src/pages/building-details/building-details.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { BuildingDetailsPage } from './building-details';
+import {NgModule} from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {BuildingDetailsPage} from './building-details';
 import {PipesModule} from '../../pipes/pipes.module';
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
-  declarations: [
-    BuildingDetailsPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(BuildingDetailsPage),
-    PipesModule
-  ],
+    declarations: [
+        BuildingDetailsPage,
+    ],
+    imports: [
+        IonicPageModule.forChild(BuildingDetailsPage),
+        PipesModule,
+        TranslateModule.forChild()
+    ],
 })
-export class BuildingDetailsPageModule {}
+export class BuildingDetailsPageModule {
+}
