db920a192a45470db56699541579604e8441c067
Philippe Robert <philippe.robert@cauca.ca>
2018-08-13 10:49:59 -0400

    feat(#15) Add localization cartography

--- src/pages/map-localization/map-localization.module.ts
+++ src/pages/map-localization/map-localization.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MapLocalizationPage } from './map-localization';
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { MapLocalizationPage } from './map-localization';
   ],
   imports: [
     IonicPageModule.forChild(MapLocalizationPage),
+      TranslateModule.forChild(),
   ],
 })
 export class MapLocalizationPageModule {}
--- src/pages/map-localization/map-localization.ts
+++ src/pages/map-localization/map-localization.ts
@@ -1,26 +1,44 @@
 import {Component, ViewChild} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
 import ol from "openlayers";
 import {MapLocalizationRepositoryService} from "../../providers/repositories/map-localisation-repository-service";
 
 @IonicPage()
 @Component({
-  selector: 'page-map-localization',
-  templateUrl: 'map-localization.html',
+    selector: 'page-map-localization',
+    templateUrl: 'map-localization.html',
 })
 export class MapLocalizationPage {
     @ViewChild('map') map;
 
-    mapLayer: ol.Map;
-    vectorSource: ol.source.Vector;
+    public mapLayer: ol.Map;
+    public vectorSource: ol.source.Vector;
+    public targetLocation: {};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-              private mapService: MapLocalizationRepositoryService) {
-  }
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                private mapService: MapLocalizationRepositoryService,
+                private viewCtrl: ViewController) {
+        if (this.navParams.get('position')) {
+            this.targetLocation = this.mapService.getCoordinatesFromGeometry(this.navParams.get('position'));
+        }
+    }
 
-  private centerMap(){
+    public ionViewDidLoad() {
+        this.loadMap();
+        this.centerMap();
+    }
 
-  }
+    private centerMap() {
+        let geometries = new ol.geom.GeometryCollection();
+        if (this.targetLocation) {
+            geometries.setGeometries([new
+            ol.geom.Point(ol.proj.transform([this.targetLocation[0], this.targetLocation[1]], 'EPSG:4326', 'EPSG:3857'))]);
+        }
+        this.mapLayer.getView().fit(geometries.getExtent());
+
+        this.refreshMap();
+    }
 
     private loadMap() {
         this.vectorSource = new ol.source.Vector({});
@@ -44,15 +62,31 @@ export class MapLocalizationPage {
             layers: [new ol.layer.Tile({source: new ol.source.OSM()}), vectorLayer],
             target: document.getElementById('map'),
             view: new ol.View({
-                maxZoom: 18,
-                minZoom:6,
+                maxZoom: 19,
+                minZoom: 6,
                 extent: [-8700000, 5600000, -7600000, 6600000]
             })
         });
     }
 
+    private addMapCentroidTarget() {
+        const custom_element = document.createElement('div');
+        custom_element.id = 'center';
+
+        this.mapLayer.addControl(new ol.control.Control({
+            element: custom_element,
+            target: document.querySelector('.ol-viewport')
+        }));
+    }
+
     private refreshMap() {
+        this.addMapCentroidTarget();
         this.mapLayer.updateSize();
+    }
+
+    public getLocalization() {
+        this.mapService.setTargetPosition(ol.proj.transform(this.mapLayer.getView().getCenter(), 'EPSG:3857', 'EPSG:4326'));
+        this.viewCtrl.dismiss();
     }
 
 }
