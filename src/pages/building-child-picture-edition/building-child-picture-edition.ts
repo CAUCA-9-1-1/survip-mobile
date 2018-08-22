56141a2978fcd530ec8d8e233c7ce29a0ddad90a
Philippe Robert <philippe.robert@cauca.ca>
2018-08-22 15:02:09 -0400

    feat(#136) remove canvas change validation

--- src/pages/building-child-picture-edition/building-child-picture-edition.ts
+++ src/pages/building-child-picture-edition/building-child-picture-edition.ts
@@ -37,7 +37,6 @@ export class BuildingChildPictureEditionPage {
 
   onCanvasChange($event) {
     this.canvas = $event;
-
   }
 
   async onOkay() {
@@ -49,16 +48,9 @@ export class BuildingChildPictureEditionPage {
 
        let imageUri = this.canvas.toDataURL();
 
-      this.picture = {id: this.picture.id, idParent: this.picture.idParent, idPicture: this.picture.idPicture, pictureData: imageUri, sketchJson: json, modified:this.isCanvasModified(json) };
+      this.picture = {id: this.picture.id, idParent: this.picture.idParent, idPicture: this.picture.idPicture, pictureData: imageUri, sketchJson: json, modified:true };
     }
     this.viewCtrl.dismiss(this.picture);
-  }
-
-  private isCanvasModified(newSketchJson){
-    if(JSON.stringify(this.sketchJson) != newSketchJson){
-        return true;
-    }
-    return false;
   }
 
   onCancel() {
