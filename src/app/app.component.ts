6d3acbbff9e53a2d4761812ff49379e4d7da4516
michael.jolin <michael.jolin@cauca.ca>
2018-09-18 11:41:28 -0400

    feat(#165): Remove are configuration from github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {TranslateService} from '@ngx-translate/core';
 import {HockeyApp} from 'ionic-hockeyapp';
+import config from '../../assets/config/config.json';
 
 @Component({
     templateUrl: 'app.html'
@@ -51,12 +52,18 @@ export class MyApp {
     }
 
     private enableHockeyApp() {
-        const androidAppId = 'bbfb228ed1114902902ed514c632a149';
-        const iosAppId = 'd08a72d4d7184a28a700d12c4061386f';
-        const autoSendCrashReports = true;
-        const ignoreCrashDialog = true;
-        this.hockeyApp.start(androidAppId, iosAppId, autoSendCrashReports, ignoreCrashDialog);
-        this.hockeyApp.trackEvent('SURVI-Prevention start');
+        const androidAppId = config.hockeyapp.androidAppId === 'YOUR_ANDROID_HOCKEYAPP_ID' ? null : config.hockeyapp.androidAppId;
+        const iosAppId = config.hockeyapp.iosAppId === 'YOUR_IOS_HOCKEYAPP_ID' ? null : config.hockeyapp.iosAppId;
+
+        if (androidAppId || iosAppId) {
+            this.hockeyApp.start(
+                androidAppId,
+                iosAppId,
+                config.hockeyapp.autoSendCrashReports,
+                config.hockeyapp.ignoreCrashDialog
+            );
+            this.hockeyApp.trackEvent('SURVI-Prevention start');
+        }
     }
 }
 
