9bcec17a71b3e3c5e9f9466ef1ff4d4ce6c42a89
Philippe Robert <philippe.robert@cauca.ca>
2018-09-19 11:38:51 -0400

    feat(#131) Hit Fix from rebase on master

--- src/pages/login-biometric/login-biometric.ts
+++ src/pages/login-biometric/login-biometric.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {KeychainTouchId} from '@ionic-native/keychain-touch-id';
 import {AuthenticationService} from '../../providers/Base/authentification.service';
-
-/**
- * Generated class for the LoginBiometricPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -23,23 +16,26 @@ export class LoginBiometricPage {
         public navParams: NavParams,
         private authService: AuthenticationService,
         private keychainTouchId: KeychainTouchId,
-        private plt: Platform,
     ) { }
 
     public ngOnInit() {
-        this.keychainTouchId.isAvailable().then(biometricType => {
-            console.log('keychain-touch-id, type', biometricType);
-        }).catch(error => {
-            console.log('keychain-touch-id', error);
+        if("cordova"in window) {
+            this.keychainTouchId.isAvailable().then(biometricType => {
+                console.log('keychain-touch-id, type', biometricType);
+            }).catch(error => {
+                console.log('keychain-touch-id', error);
 
-            if (error === 'plugin_not_installed') {
-                setTimeout(() => {
-                    this.ngOnInit();
-                }, 100);
-            } else {
-                this.dontUseBiometric();
-            }
-        });
+                if (error === 'plugin_not_installed') {
+                    setTimeout(() => {
+                        this.ngOnInit();
+                    }, 100);
+                } else {
+                    this.dontUseBiometric();
+                }
+            });
+        }else{
+            this.dontUseBiometric();
+        }
     }
 
     public useBiometric() {
