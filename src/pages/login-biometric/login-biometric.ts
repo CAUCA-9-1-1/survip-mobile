4cdfa320d62d6703c63d883ae627cc9661c58445
Philippe Robert <philippe.robert@cauca.ca>
2018-09-19 11:38:51 -0400

    Revert "feat(#131) Hit Fix from rebase on master"
    This reverts commit 3347b628ae93163ab019ff36c7aa2e8aac5ac760.

--- src/pages/login-biometric/login-biometric.ts
+++ src/pages/login-biometric/login-biometric.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
 import {KeychainTouchId} from '@ionic-native/keychain-touch-id';
 import {AuthenticationService} from '../../providers/Base/authentification.service';
+
+/**
+ * Generated class for the LoginBiometricPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
 @IonicPage()
 @Component({
@@ -16,26 +23,23 @@ export class LoginBiometricPage {
         public navParams: NavParams,
         private authService: AuthenticationService,
         private keychainTouchId: KeychainTouchId,
+        private plt: Platform,
     ) { }
 
     public ngOnInit() {
-        if("cordova"in window) {
-            this.keychainTouchId.isAvailable().then(biometricType => {
-                console.log('keychain-touch-id, type', biometricType);
-            }).catch(error => {
-                console.log('keychain-touch-id', error);
+        this.keychainTouchId.isAvailable().then(biometricType => {
+            console.log('keychain-touch-id, type', biometricType);
+        }).catch(error => {
+            console.log('keychain-touch-id', error);
 
-                if (error === 'plugin_not_installed') {
-                    setTimeout(() => {
-                        this.ngOnInit();
-                    }, 100);
-                } else {
-                    this.dontUseBiometric();
-                }
-            });
-        }else{
-            this.dontUseBiometric();
-        }
+            if (error === 'plugin_not_installed') {
+                setTimeout(() => {
+                    this.ngOnInit();
+                }, 100);
+            } else {
+                this.dontUseBiometric();
+            }
+        });
     }
 
     public useBiometric() {
