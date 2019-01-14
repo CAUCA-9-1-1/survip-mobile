546f9f9691f105948256c414097046794f6aaf82
Frederic Catellier <syd_freak@hotmail.com>
2019-01-14 08:04:36 -0500

    Fix(#298) Make orderBy pipe working when used with non string columns.

--- src/pipes/order-by/order-by.ts
+++ src/pipes/order-by/order-by.ts
@@ -13,7 +13,7 @@ export class OrderByPipe implements PipeTransform {
    * Takes an array and sort it by the field.
    */
   transform(list: any[], field: string): any[] {
-    let sorted = list.sort((t1, t2) => t1[field].localeCompare(t2[field]));
+    let sorted = list.sort((t1, t2) => t1[field].toString().localeCompare(t2[field].toString()));
     return sorted;
   }
 }
