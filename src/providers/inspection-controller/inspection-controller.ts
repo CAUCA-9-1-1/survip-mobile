95537bf6eee11408ebce6cf0e3d47836673c5409
Frederic Catellier <syd_freak@hotmail.com>
2019-01-16 11:39:03 -0500

    Feat(#301) Allow user to modify transversal offline.

--- src/providers/inspection-controller/inspection-controller.ts
+++ src/providers/inspection-controller/inspection-controller.ts
@@ -13,87 +13,106 @@ import {InspectionConfigurationProvider} from "../inspection-configuration/inspe
 import {InspectionDataSynchronizerProvider} from "../inspection-data-synchronizer/inspection-data-synchronizer";
 
 @Injectable()
-export class InspectionControllerProvider{
+export class InspectionControllerProvider {
 
-    public currentInspection: Inspection;
-    public idInspection: string;
-    public buildings: InspectionBuildingForList[];
-    public inspection: InspectionWithBuildingsList;
-    public planLoaded: EventEmitter<any> = new EventEmitter<any>();
-    public labels = {};
+  public currentInspection: Inspection;
+  public idInspection: string;
+  public buildings: InspectionBuildingForList[];
+  public inspection: InspectionWithBuildingsList;
+  public planLoaded: EventEmitter<any> = new EventEmitter<any>();
+  public labels = {};
 
-    constructor(
-        private configController: InspectionConfigurationProvider,
-        private repoInspection: InspectionRepositoryProvider,
-        private repoDetail: InspectionDetailRepositoryProvider,
-        private repoBuildings: InspectionBuildingsRepositoryProvider,
-        private loadingCtrl: LoadingController,
-        private laneRepo: LaneRepositoryProvider,
-        private dataRepoInspection: InspectionDataSynchronizerProvider,
-        private buildingFireHydrantRepo: BuildingFireHydrantRepositoryProvider) {
-    }
+  constructor(
+    private configController: InspectionConfigurationProvider,
+    private repoInspection: InspectionRepositoryProvider,
+    private repoDetail: InspectionDetailRepositoryProvider,
+    private repoBuildings: InspectionBuildingsRepositoryProvider,
+    private loadingCtrl: LoadingController,
+    private laneRepo: LaneRepositoryProvider,
+    private dataRepoInspection: InspectionDataSynchronizerProvider,
+    private buildingFireHydrantRepo: BuildingFireHydrantRepositoryProvider) {
+  }
 
-    public async setIdInspection(idInspection: string) {
-      if (this.idInspection != idInspection) {
-        console.log('change inspection');
+  public async setIdInspection(idInspection: string): Promise<boolean> {
+    if (this.idInspection != idInspection) {
+      console.log('change inspection');
+      const successfullyLoaded: boolean = await this.loadInspection(idInspection);
+      if (successfullyLoaded){
         this.idInspection = idInspection;
-        await this.loadInspection();
       }
+      return successfullyLoaded;
     }
+    return true;
+  }
 
-    private async loadInspection(): Promise<boolean> {
-        this.currentInspection = null;
-        const loading = this.createLoadingControl();
-        await loading.present();
-        this.currentInspection = await this.repoInspection.getResumedInspection(this.idInspection);
-        this.inspection = await this.repoInspection.getInspection(this.idInspection);
+  private loadInspection(idInspection: string): Promise<boolean> {
+    return new Promise(async (resolve) => {
+      this.currentInspection = null;
+      const loading = this.createLoadingControl();
+      await loading.present();
+      this.currentInspection = await this.repoInspection.getResumedInspection(idInspection);
+      this.inspection = await this.repoInspection.getInspection(idInspection);
 
-        if (this.inspection == null){
-          console.log('get from database');
-          this.inspection = await this.dataRepoInspection.getInspection(this.idInspection).toPromise()
-        }
-
-        console.log('inspection', this.inspection, this.currentInspection);
-
-        await this.configController.setConfiguration(this.inspection.configuration);
-
-        await this.laneRepo.setCurrentIdCity(this.currentInspection.idCity);
+      if (this.inspection == null) {
+        return this.dataRepoInspection.getInspection(idInspection).subscribe(
+          async (inspection) => {
+            this.inspection = inspection;
+            await this.loadLanesAndSetConfiguration(loading);
+            await loading.dismiss();
+            resolve(true);
+          },
+          async () => {
+            await loading.dismiss();
+            resolve(false);
+          }
+        )
+      } else {
+        await this.loadLanesAndSetConfiguration(loading);
         await loading.dismiss();
-        return Promise.resolve(true);
-    }
+        resolve(true);
+      }
+    });
+  }
 
-    public loadBuildingList() {
-        const loading = this.createLoadingControl();
-        loading.present();
-        const result = this.repoBuildings.get(this.idInspection);
-        result.subscribe(data => {
-            this.buildings = data as InspectionBuildingForList[];
-            loading.dismiss();
-        },
-          () => loading.dismiss());
-    }
+  private async loadLanesAndSetConfiguration(loading) {
+    await this.configController.setConfiguration(this.inspection.configuration);
+    await this.laneRepo.setCurrentIdCity(this.currentInspection.idCity);
+    await loading.dismiss();
+  }
 
-    private createLoadingControl() {
-        return this.loadingCtrl.create({content: this.labels['loading']});
-    }
+  public loadBuildingList() {
+    const loading = this.createLoadingControl();
+    loading.present();
+    const result = this.repoBuildings.get(this.idInspection);
+    result.subscribe(data => {
+        this.buildings = data as InspectionBuildingForList[];
+        loading.dismiss();
+      },
+      () => loading.dismiss());
+  }
 
-    public savePlanIdPicture(idPicture: string) {
-/*        this.inspectionDetail.idPictureSitePlan = idPicture;
-        this.repoDetail.savePicture(this.inspectionDetail.idDetail, this.inspectionDetail.idPictureSitePlan)
-            .subscribe(ok => {
-                console.log("Picture saved", ok);
-            });*/
-    }
+  private createLoadingControl() {
+    return this.loadingCtrl.create({content: this.labels['loading']});
+  }
 
-    public savePlanTransversal() {
-        /*this.repoDetail.savePlanLane(this.inspectionDetail.id, this.inspectionDetail.idLaneTransversal)
-            .subscribe(ok => {
-                console.log("Plan saved", ok);
-            });*/
-    }
+  public savePlanIdPicture(idPicture: string) {
+    /*        this.inspectionDetail.idPictureSitePlan = idPicture;
+            this.repoDetail.savePicture(this.inspectionDetail.idDetail, this.inspectionDetail.idPictureSitePlan)
+                .subscribe(ok => {
+                    console.log("Picture saved", ok);
+                });*/
+  }
 
-    public deleteBuildingFireHydrant(idBuildingFireHydrant: string) {
-        return this.buildingFireHydrantRepo.deleteBuildingFireHydrant(idBuildingFireHydrant)
-            .pipe(map(response => response));
-    }
+  public getMainBuilding(): InspectionBuildingForList{
+      return this.inspection.buildings.filter(building => building.isMainBuilding)[0];
+  }
+
+  public saveBuildings() {
+      this.repoInspection.save(this.inspection);
+  }
+
+  public deleteBuildingFireHydrant(idBuildingFireHydrant: string) {
+    return this.buildingFireHydrantRepo.deleteBuildingFireHydrant(idBuildingFireHydrant)
+      .pipe(map(response => response));
+  }
 }
