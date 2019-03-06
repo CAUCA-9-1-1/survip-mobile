08db742f80c9584c7b47da7b551f6deb4a9966a1
Frederic Catellier <syd_freak@hotmail.com>
2019-03-06 10:10:24 -0500

    Feat(#306) inpsection uploader now correctly manages building's fire hydrants and survey answers.

--- src/providers/inspection-uploader/inspection-uploader.ts
+++ src/providers/inspection-uploader/inspection-uploader.ts
@@ -16,8 +16,10 @@ export class InspectionUploaderProvider {
     const inspection: InspectionWithBuildingsList = await this.storage.get('inspection_buildings_' + idInspection);
     if (inspection != null) {
       const promises: Promise<boolean>[] = [];
+      promises.push(this.saveInspectionSurveyAnswers(idInspection));
       inspection.buildings.forEach(building => {
         promises.push(this.saveSingleEntity(building, ''));
+        promises.push(this.saveBuildingHydrants(building.idBuilding));
         promises.push(this.savePictures(building.idBuilding, 'building_plan_picture_', building.idBuilding + '/detail/picture'));
         promises.push(this.saveEntity(building.idBuilding, 'building_detail_', 'detail'));
         promises.push(this.saveEntities(building.idBuilding, 'building_contacts_', 'contact'));
@@ -33,7 +35,7 @@ export class InspectionUploaderProvider {
         promises.push(this.saveParticularRisk(building.idBuilding, 'roof'));
       });
       const success = await Promise.all(promises)
-        .then(responses => responses.every(r => r))
+        .then(responses => responses.every(r => r));
 
       if (success) {
         return this.storage.set('inspection_buildings_' + idInspection, inspection);
@@ -44,6 +46,60 @@ export class InspectionUploaderProvider {
     } else {
       return false;
     }
+  }
+
+  private async saveBuildingHydrants(idBuilding: string): Promise<boolean>{
+    const entities = await this.storage.get('building_fire_hydrants_' + idBuilding);
+    if (entities != null && entities.length > 0) {
+      const ids = entities.map(hydrant => hydrant.idFireHydrant);
+      if (await this.sendToApi(ids, idBuilding + '/firehydrants')) {
+        entities.forEach(hydrant => hydrant.hasBeenModified = false);
+        return await this.storage.set('building_fire_hydrants_' + idBuilding, entities);
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
+
+  private async saveInspectionSurveyAnswers(idInspection: string): Promise<boolean> {
+    const entities = await this.storage.get('inspection_survey_answers_' + idInspection);
+    const answersToSave = [];
+    if (entities != null && entities.length > 0) {
+      entities.forEach(answer => {
+
+        if (answer.questionType == 4) {
+
+          const childAnswers = answer.childSurveyAnswerList.filter(a => a.answer != null);
+          if (childAnswers.length > 0) {
+            answersToSave.push(answer);
+            childAnswers.forEach(childAnswer => {
+              if (childAnswer.answer != null) {
+                answersToSave.push(childAnswer);
+              }
+            })
+          }
+        } else if (answer.answer != null) {
+          answersToSave.push(answer);
+        }
+      });
+
+      console.log('answers to save', answersToSave);
+      return await this.sendAnswersToApi(idInspection, answersToSave);
+    } else {
+      return true;
+    }
+  }
+
+  private sendAnswersToApi(idInspection: string, answers): Promise<boolean>{
+    return  new Promise((resolve) => {
+      this.httpService.post('Inspection/' + idInspection + '/Answers', JSON.stringify(answers))
+        .subscribe(
+          () => resolve(true),
+          () => resolve(false)
+        );
+    });
   }
 
   private async saveEntities(idBuilding: string, key: string, url: string, pictureKey?: string, pictureUrl?: string): Promise<boolean> {
@@ -88,7 +144,7 @@ export class InspectionUploaderProvider {
   }
 
   private async saveSingleEntity(entity, url: string, pictureKey?: string, pictureUrl?: string): Promise<boolean> {
-    console.log('has been modified', url, entity.hasBeenModified);
+    console.log('has been modified', url, entity);
     if (entity.hasBeenModified) {
       const hasBeenSaved = await this.sendToApi(entity, url);
       if (hasBeenSaved) {
