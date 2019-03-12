7a585f64503ec4b0e8c9e75ecef7a513ceb49fd2
Frederic Catellier <syd_freak@hotmail.com>
2019-03-12 08:32:45 -0400

    Feat(#302)  Allow user to download a full batch.

--- src/models/batch.ts
+++ src/models/batch.ts
@@ -4,4 +4,7 @@ export class Batch {
   public id: string;
   public description: string;
   public inspections: Inspection[];
+
+  public hasBeenFullyDownloaded: boolean;
+  public notDownloadedInspectionIds: string[];
 }
