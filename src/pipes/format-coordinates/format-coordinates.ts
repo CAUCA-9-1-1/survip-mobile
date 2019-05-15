a85ed27d974310ec9e253c07f3a86d5c207ad805
Frederic Catellier <syd_freak@hotmail.com>
2019-05-15 07:47:12 -0400

    Fix(#331) Fix coordinates formatting (without open layers).

--- src/pipes/format-coordinates/format-coordinates.ts
+++ src/pipes/format-coordinates/format-coordinates.ts
@@ -1,5 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-//import ol from "openlayers";
 
 @Pipe({
   name: 'formatCoordinates',
@@ -8,20 +7,9 @@ export class FormatCoordinatesPipe implements PipeTransform {
 
     transform(value: string) {
         if (value && value.startsWith('POINT (') && value.endsWith(')')) {
-            const substring = value.substr(7, value.length - 2);
-            console.log('oh my', substring);
+            const substring = value.substr(7, value.length - 8);
             const values = substring.split(' ');
-            console.log('oh my god', values);
             return "[" + values[1] + ", " + values[0] + "]";
-
-            /*if (value) {
-                let formatCoords = new ol.format.WKT().readGeometry(value);
-                if (formatCoords) {
-                    let long = parseFloat(formatCoords['A'][0]).toPrecision(8);
-                    let lat = parseFloat(formatCoords['A'][1]).toPrecision(8);
-                    return "[" + lat + ", " + long + "]";
-                }
-            }*/
         }
         return value;
     }
