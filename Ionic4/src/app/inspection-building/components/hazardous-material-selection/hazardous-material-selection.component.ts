df1cd2ea21f7e4c50eb8e4faca13d86bdbff6fb7
Frederic Catellier <syd_freak@hotmail.com>
2019-07-22 12:22:55 -0400

    Feat(#295) Add all inspection's building components.

--- Ionic4/src/app/inspection-building/components/hazardous-material-selection/hazardous-material-selection.component.ts
+++ Ionic4/src/app/inspection-building/components/hazardous-material-selection/hazardous-material-selection.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HazardousMaterialForList } from 'src/app/shared/models/hazardous-material-for-list';
+import { ModalController } from '@ionic/angular';
+import { HazardousMaterialRepositoryProvider } from 'src/app/core/services/repositories/hazardous-material-repository';
 
 @Component({
   selector: 'app-hazardous-material-selection',
@@ -7,8 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HazardousMaterialSelectionComponent implements OnInit {
 
-  constructor() { }
+  public searchTerm: string = '';
+  public hazardousMaterials: HazardousMaterialForList[] = [];
 
-  ngOnInit() {}
+  constructor(
+    private matRepo: HazardousMaterialRepositoryProvider,
+    private modalController: ModalController
+  ) {
+    this.matRepo.getFiltered(this.searchTerm.trim())
+      .then(materials => this.hazardousMaterials = materials);
+  }
 
+  ngOnInit() { }
+
+  public onSearch() {
+    this.matRepo.getFiltered(this.searchTerm.trim())
+      .then(materials => this.hazardousMaterials = materials);
+  }
+
+  public onSelectMaterial(material: HazardousMaterialForList) {
+    this.modalController.dismiss({ hasSelected: true, selectedMaterial: material });
+  }
+
+  public onCancel() {
+    this.modalController.dismiss({ hasSelected: false });
+  }
 }
